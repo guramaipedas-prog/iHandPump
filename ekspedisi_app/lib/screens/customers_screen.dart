fda2e9e4760d377fe6eda0b6c44ce9ec6472ad24
HandPump <guramaipedas@gmail.com>
2026-05-10 15:41:15 +0700

    fix: add missing import for FilteringTextInputFormatter

--- ekspedisi_app/lib/screens/customers_screen.dart
+++ ekspedisi_app/lib/screens/customers_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../providers/app_provider.dart';
 import '../models/customer.dart';
